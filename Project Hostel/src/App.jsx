27a4b37be8928cf13b08f41d3661ea995208436e
yashsehgal29 <yashsehgal2974@gmail.com>
2025-01-08 23:09:40 +0530

    Modified the directores and files installed material-tailwind tailwindcss react-router-dom created a bit of navbar for teh first page/Homepage and created a rough routes

--- Project Hostel/src/App.jsx
+++ Project Hostel/src/App.jsx
@@ -1,1 +1,29 @@
-// main app component
+//  app component'
+import { useState } from "react";
+import { Button } from "@material-tailwind/react";
+import "./App.css";
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import OwnerDashboard from "./pages/OwnerDashboard";
+import UserDashboard from "./pages/UserDashboard";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    //full containers
+    //and route mangement
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/owner" element={<OwnerDashboard />} />
+          <Route path="/user" element={<UserDashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
