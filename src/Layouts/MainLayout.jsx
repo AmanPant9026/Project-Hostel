2b387b1418399682919d9deea33ba4d614a4f78f
AMAN <pantaman98@gmail.com>
2025-02-04 09:24:21 +0530

    added main layout and inner layout

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,1 +1,9 @@
-// Layout for login/register pages
+import styled from "styled-components";
+
+export const MainLayout=styled.div`
+    padding:3rem ;
+`;
+
+export const InnerLayout=styled.div`
+    padding:2rem 0;
+`;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,50 @@
+import React, { useState, useEffect } from "react";
 import { Button } from "@material-tailwind/react";
-import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 400) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="flex items-center justify-between w-screen px-10 py-3 bg-gray-100 shadow-lg">
+    <div
+      className={`fixed z-20 flex items-center justify-between w-screen px-10 py-2 shadow-lg transition-colors duration-300 ${
+        isScrolled ? "bg-white text-black" : "bg-transparent text-white"
+      }`}
+    >
       {/* Logo */}
-      <Link
-        to="/"
-        className="text-2xl font-extrabold text-[#3164f4] font-poppins"
-      >
+      <Link to="/" className="text-xl font-extrabold font-playwrite">
         Hostel Stay
       </Link>
 
       {/* Login / Signup */}
-      <div className="flex gap-2">
+      <div className="flex items-center justify-center gap-2">
         <Link to="/login">
-          <Button variant="text" className="font-poppins">
+          <Button
+            variant="text"
+            className={`px-4 py-2 text-sm font-medium capitalize font-source ${
+              isScrolled ? "text-black" : "text-white"
+            }`}
+          >
             Login
           </Button>
         </Link>
-      
+       
       </div>
     </div>
   );
